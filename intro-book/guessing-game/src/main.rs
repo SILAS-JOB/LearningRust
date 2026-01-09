eafe53f2f44a198b041b97dffb5f49a5daed2278
SILAS-JOB <tiilas777@gmail.com>
2026-01-09 18:44:43 +0000

    feat: gg with rand

--- intro-book/guessing-game/src/main.rs
+++ intro-book/guessing-game/src/main.rs
@@ -1,14 +1,36 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Let's play the guessing game !!!!!");
-    println!("Guess a number : ");
 
-    let mut guess = String::new();
+    loop {
+        let secret_number = rand::rng().random_range(1..=100);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Could'nt read line ");
+        println!("Guess a number : ");
 
-    println!("Im guessing the number ... {guess}")
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Couldn't read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("Your number is {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Way below, the random number is {secret_number}"),
+            Ordering::Greater => println!("Way above, the random number is {secret_number}"),
+            Ordering::Equal => {
+                println!(" You Win ! ");
+                break;
+            }
+        }
+    }
+    println!("Guess a number : ")
 }
