691c4ba2f63a10742f39a55efb1b8d01ad7fea32
SILAS-JOB <tiilas777@gmail.com>
2026-01-19 20:44:27 -0300

    test

--- intro-book/tests/src/lib.rs
+++ intro-book/tests/src/lib.rs
@@ -1,14 +1,13 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn add_two(a: u64) -> u64 {
+    a + 2
 }
 
 #[cfg(test)]
-mod tests {
+mod test {
     use super::*;
-
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
+    fn add_two_and_two() {
+        let result = add_two(2);
         assert_eq!(result, 4);
     }
 }
