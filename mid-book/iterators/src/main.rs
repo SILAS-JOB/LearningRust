99ecc4de6a6a5d664f493eaac411b6314df92ef2
SILAS-JOB <tiilas777@gmail.com>
2026-01-24 17:56:01 -0300

    feat: iterators

---
+++ mid-book/iterators/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let v1 = vec![1, 2, 3];
+
+    let v1_itr = v1.iter();
+    for i in v1 {
+        println!("{i}");
+    }
+}
---
+++ mid-book/smart-pointers/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let b = Box::new("pilas");
+    println!("b = {b}");
+}
