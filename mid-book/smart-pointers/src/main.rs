2a108051d3f8e42f91dc2d5f87e29b1e57bc1225
SILAS-JOB <tiilas777@gmail.com>
2026-01-24 20:09:30 -0300

    feat: Box pointer

--- mid-book/smart-pointers/src/main.rs
+++ mid-book/smart-pointers/src/main.rs
@@ -1,4 +1,26 @@
+//COns List ds
+// enum List {
+//     Cons(i32, List),
+//     Nil,
+// }
+//
+// use crate::List::{Cons, Nil};
+//
+// fn main() {
+//     let list = Cons(1, Cons(2, Cons(3, Nil)));
+//     let x = Box::new(5);
+// }
+//
+//
+
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+
 fn main() {
-    let b = Box::new("pilas");
-    println!("b = {b}");
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    let x = Box::new(5);
 }
